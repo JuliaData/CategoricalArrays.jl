37fa24af6fe09a03e8683fb270b5e43b0c047c83
Dave Kleinschmidt <dkleinschmidt@bcs.rochester.edu>
2016-10-07 08:35:22 -0400

    test nullable categorical array and use ===

--- test/14_view.jl
+++ test/14_view.jl
@@ -3,12 +3,14 @@ module TestView
 using Base.Test
 using CategoricalArrays
 
-for order in [true, false]
-    x = categorical(collect(1:10), ordered=order)
-    for inds in [1:2, :, 1, []]
-        v = view(x, inds)
-        @test levels(v) == levels(x)
-        @test isordered(v) == isordered(x)
+for T in [CategoricalArray, NullableCategoricalArray]
+    for order in [true, false]
+        x = T(1:10, ordered=order)
+        for inds in [1:2, :, 1, []]
+            v = view(x, inds)
+            @test levels(v) === levels(x)
+            @test isordered(v) === isordered(x)
+        end
     end
 end
 
