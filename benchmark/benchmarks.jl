4f13f47a764b9b02d8ccddb1d03f71dd5b2f5eff
Milan Bouchet-Valat <nalimilan@club.fr>
2017-02-22 15:36:34 +0100

    Improve benchmarks
    The NullableCategoricalArray did not actually contain any null value. Also
    test isqual(::CategoricalValue, ::CategoricalValue).

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,7 +1,7 @@
 using PkgBenchmark
 using CategoricalArrays
 
-@benchgroup "isequal" begin
+@benchgroup "isequal(A, v::String)" begin
     function sumequals(A::AbstractArray, v::Any)
         n = 0
         @inbounds for x in A
@@ -11,7 +11,22 @@ using CategoricalArrays
     end
 
     ca = CategoricalArray(repeat(string.('A':'J'), outer=1000))
-    nca = NullableCategoricalArray(repeat(string.([Nullable(); 'A':'J']), outer=1000))
+    nca = NullableCategoricalArray(repeat([Nullable(); string.('A':'J')], outer=1000))
     @bench "CategoricalArray" sumequals(ca, "D")
     @bench "NullableCategoricalArray" sumequals(nca, Nullable("D"))
+end
+
+@benchgroup "isequal(A, v::CategoricalValue)" begin
+    function sumequals(A::AbstractArray, v::CategoricalValue)
+        n = 0
+        @inbounds for x in A
+            n += isequal(x, v)
+        end
+        n
+    end
+
+    ca = CategoricalArray(repeat(string.('A':'J'), outer=1000))
+    nca = NullableCategoricalArray(repeat([Nullable(); string.('A':'J')], outer=1000))
+    @bench "CategoricalArray" sumequals(ca, ca[1])
+    @bench "NullableCategoricalArray" sumequals(nca, nca[1])
 end
