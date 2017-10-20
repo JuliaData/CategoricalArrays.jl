78048ebae800eb09e3d432e6deb73cc7031a08ef
Alexey Stukalov <astukalov@gmail.com>
2017-10-20 14:28:06 +0200

    fix typo

--- src/pool.jl
+++ src/pool.jl
@@ -4,7 +4,7 @@ function CategoricalPool(index::Vector{S},
                          ordered::Bool=false) where {S, T <: Integer, R <: Integer}
     invindex = convert(Dict{S, R}, invindex)
     C = catvalue_type(S, R)
-    V = valtype(C) # might be diffrent from S (e.g. S==SubString, V==String)
+    V = valtype(C) # might be different from S (e.g. S == SubString, V == String)
     CategoricalPool{V, R, C}(index, invindex, order, ordered)
 end
 
