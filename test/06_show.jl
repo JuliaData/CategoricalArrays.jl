75f0855864894ba1d0a1ebcdccacf4cf8a6c2ec1
Alexey Stukalov <astukalov@gmail.com>
2017-10-21 18:42:52 +0200

    test show() for Date-value categories and pool

--- test/06_show.jl
+++ test/06_show.jl
@@ -40,4 +40,45 @@ module TestShow
     @test repr(nv1) == repr(ov1) == "\"c\""
     @test repr(nv2) == repr(ov2) == "\"b\""
     @test repr(nv3) == repr(ov3) == "\"a\""
+
+    # test Date-valued categories pool
+    pool = CategoricalPool([Date(1999, 12), Date(1991, 8), Date(1993, 10)])
+
+    opool = CategoricalPool([Date(1999, 12), Date(1991, 8), Date(1993, 10)],
+                            [Date(1991, 8), Date(1993, 10), Date(1999, 12)], true)
+
+    nv1 = CategoricalArrays.catvalue(1, pool)
+    nv2 = CategoricalArrays.catvalue(2, pool)
+    nv3 = CategoricalArrays.catvalue(3, pool)
+
+    ov1 = CategoricalArrays.catvalue(1, opool)
+    ov2 = CategoricalArrays.catvalue(2, opool)
+    ov3 = CategoricalArrays.catvalue(3, opool)
+
+    @test sprint(show, pool) == "CategoricalArrays.CategoricalPool{Date,UInt32}([1999-12-01,1991-08-01,1993-10-01])"
+    @test sprint(show, opool) == "CategoricalArrays.CategoricalPool{Date,UInt32}([1991-08-01,1993-10-01,1999-12-01]) with ordered levels"
+
+    @test sprint(show, nv1) == "CategoricalArrays.CategoricalValue{Date,UInt32} 1999-12-01"
+    @test sprint(show, nv2) == "CategoricalArrays.CategoricalValue{Date,UInt32} 1991-08-01"
+    @test sprint(show, nv3) == "CategoricalArrays.CategoricalValue{Date,UInt32} 1993-10-01"
+
+    @test sprint(show, ov1) == "CategoricalArrays.CategoricalValue{Date,UInt32} 1999-12-01 (3/3)"
+    @test sprint(show, ov2) == "CategoricalArrays.CategoricalValue{Date,UInt32} 1991-08-01 (1/3)"
+    @test sprint(show, ov3) == "CategoricalArrays.CategoricalValue{Date,UInt32} 1993-10-01 (2/3)"
+
+    @test sprint(showcompact, nv1) == sprint(showcompact, ov1) == "1999-12-01"
+    @test sprint(showcompact, nv2) == sprint(showcompact, ov2) == "1991-08-01"
+    @test sprint(showcompact, nv3) == sprint(showcompact, ov3) == "1993-10-01"
+
+    @test sprint(print, nv1) == sprint(print, ov1) == "1999-12-01"
+    @test sprint(print, nv2) == sprint(print, ov2) == "1991-08-01"
+    @test sprint(print, nv3) == sprint(print, ov3) == "1993-10-01"
+
+    @test string(nv1) == string(ov1) == "1999-12-01"
+    @test string(nv2) == string(ov2) == "1991-08-01"
+    @test string(nv3) == string(ov3) == "1993-10-01"
+
+    @test repr(nv1) == repr(ov1) == "1999-12-01"
+    @test repr(nv2) == repr(ov2) == "1991-08-01"
+    @test repr(nv3) == repr(ov3) == "1993-10-01"
 end
