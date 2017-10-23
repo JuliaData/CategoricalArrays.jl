d6661904ee809bc7bdd40c83a271f3637ea7d063
Milan Bouchet-Valat <nalimilan@club.fr>
2017-10-23 11:34:54 +0200

    Remove commented out constructors

--- src/nullablearray.jl
+++ src/nullablearray.jl
@@ -3,41 +3,6 @@ import Base: convert, getindex, setindex!, similar, in
 ## Constructors and converters
 ## (special methods for AbstractArray{Union{T, Null}}, to avoid wrapping nulls inside CategoricalValues)
 
-# FIXME remove these ctors? (handled by generic CategoricalArray ctors) 
-#CategoricalArray{Union{T, Null}, N, R}(dims::NTuple{N,Int}; ordered=false) where {T, N, R<:Integer} =
-#    CategoricalArray{Union{T, Null}, N}(zeros(R, dims), CategoricalPool{T, R}(ordered))
-
-#function CategoricalArray{Union{T, Null}, N, R}(dims::NTuple{N,Int};
-#                                                ordered=false) where {T<:CatValue, N, R<:Integer}
-#    V = unwrap_catvaluetype(T)
-#    CategoricalArray{Union{V, Null}, N}(zeros(R, dims), CategoricalPool{V, R, T}(ordered))
-#end
-
-#CategoricalArray{Union{T, Null}, N}(dims::NTuple{N,Int}; ordered=false) where {T<:CatValue, N} =
-#    CategoricalArray{Union{T, Null}, N, reftype(T)}(dims, ordered=ordered)
-
-#CategoricalArray{Union{T, Null}}(dims::NTuple{N,Int}; ordered=false) where {T<:CatValue, N} =
-#    CategoricalArray{Union{T, Null}, N}(dims, ordered=ordered)
-
-# CategoricalArray{Union{CategoricalValue, Null}, N}}){N}(dims::NTuple{N,Int};
-#                                                                 ordered=false) =
-#     CategoricalArray{Union{String, Null}, N}(dims; ordered=ordered)
-# CategoricalArray{Union{CategoricalValue, Null}}}){N}(dims::NTuple{N,Int};
-#                                                              ordered=false) =
-#     CategoricalArray{Union{String, Null}, N}(dims; ordered=ordered)
-
-#CategoricalVector{T}(m::Int; ordered=false) where {T>:Null} =
-#    CategoricalArray{T}((m,); ordered=ordered)
-#CategoricalMatrix{T}(m::Int, n::Int; ordered=false) where {T>:Null} =
-#    CategoricalArray{T}((m, n); ordered=ordered)
-
-#CategoricalArray(A::AbstractArray{T}; ordered=_isordered(A)) where {T>:Null} =
-#    CategoricalArray{T}(A, ordered=ordered)
-#CategoricalVector(A::AbstractVector{T}; ordered=_isordered(A)) where {T>:Null} =
-#    CategoricalVector{T}(A, ordered=ordered)
-#CategoricalMatrix(A::AbstractMatrix{T}; ordered=_isordered(A)) where {T>:Null} =
-#    CategoricalMatrix{T}(A, ordered=ordered)
-
 function convert(::Type{CategoricalArray{T, N, R}}, A::AbstractArray{S, N}) where {S, T>:Null, N, R}
     res = CategoricalArray{T, N, R}(size(A))
     copy!(res, A)
