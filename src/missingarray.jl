9d63ee30c858a8505eaffe6e513c0730249f7042
Johan Gustafsson <johan.gustafsson@campanja.com>
2018-02-02 14:40:14 +0100

    more efficient collect

--- src/missingarray.jl
+++ src/missingarray.jl
@@ -33,6 +33,6 @@ function Missings.replace(a::CategoricalArray{S, N, R, V, C}, replacement::V) wh
     Missings.replace(a, v)
 end
 
-function collect(r::Missings.EachReplaceMissing{CategoricalArray{S, N, R, C, V, U},V}) where {S, N, R, C, V, U}
-           CategoricalArray(C[v for v in r])
+function collect(r::Missings.EachReplaceMissing{CategoricalArray{S, N, R, C, V, U}, V}) where {S, N, R, C, V, U}
+    CategoricalArray{C,N}([v.level for v in r], r.replacement.pool)
 end
