a062cd9852cd8ed03780e36f13870bb277a10d81
Milan Bouchet-Valat <nalimilan@club.fr>
2019-11-22 18:35:30 +0100

    Fix copying valindex
    Values must refer to the new pool.

--- test/05_copy.jl
+++ test/05_copy.jl
@@ -13,6 +13,7 @@ using CategoricalArrays: CategoricalPool, catvalue
     @test pool2.invindex == Dict("d"=>1, "c"=>2, "b"=>3)
     @test pool2.order == 1:3
     @test pool2.valindex == [catvalue(i, pool2) for i in 1:3]
+    @test all(v -> v.pool === pool2, pool2.valindex)
     @test pool2.ordered
 
     levels!(pool2, ["a", "b", "c", "d"])
@@ -23,6 +24,7 @@ using CategoricalArrays: CategoricalPool, catvalue
     @test pool.invindex == Dict("d"=>1, "c"=>2, "b"=>3)
     @test pool.order == 1:3
     @test pool.valindex == [catvalue(i, pool) for i in 1:3]
+    @test all(v -> v.pool === pool, pool.valindex)
     @test pool.ordered
 end
 
