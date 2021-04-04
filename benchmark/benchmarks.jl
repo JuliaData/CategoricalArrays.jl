008bb6109ac3cf7093cf179686feb7b6554b369d
Milan Bouchet-Valat <nalimilan@club.fr>
2021-04-04 21:27:06 +0200

    Adapt to new PkgBenchmark syntax (#339)

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,67 +1,89 @@
-using PkgBenchmark
+using BenchmarkTools
 using CategoricalArrays
 using Printf
 
-@benchgroup "isequal(A, v::String)" begin
-    function sumequals(A::AbstractArray, v::Any)
-        n = 0
-        @inbounds for x in A
-            n += isequal(x, v)
-        end
-        n
-    end
+const SUITE = BenchmarkGroup()
 
-    ca = CategoricalArray(repeat(string.('A':'J'), outer=1000))
-    nca = CategoricalArray(repeat([missing; string.('A':'J')], outer=1000))
-    @bench "CategoricalArray{String}" sumequals(ca, "D")
-    @bench "CategoricalArray{Union{String, Missing}}" sumequals(nca, "D")
+SUITE["isequal"] = BenchmarkGroup()
+
+function sumequals(A::AbstractArray, v::Any)
+    n = 0
+    @inbounds for x in A
+        n += isequal(x, v)
+    end
+    n
 end
 
-@benchgroup "isequal(A, v::CategoricalValue)" begin
-    function sumequals(A::AbstractArray, v::CategoricalValue)
-        n = 0
-        @inbounds for x in A
-            n += isequal(x, v)
-        end
-        n
-    end
+ca = CategoricalArray(repeat(string.('A':'J'), outer=1000))
+nca = CategoricalArray(repeat([missing; string.('A':'J')], outer=1000))
 
-    ca = CategoricalArray(repeat(string.('A':'J'), outer=1000))
-    nca = CategoricalArray(repeat([missing; string.('A':'J')], outer=1000))
-    @bench "CategoricalArray{String}" sumequals(ca, ca[1])
-    @bench "CategoricalArray{Union{String, Missing}}" sumequals(nca, nca[1])
+SUITE["isequal"]["CategoricalArray{String}", "String"] =
+    @benchmarkable  sumequals(ca, "D")
+SUITE["isequal"]["CategoricalArray{Union{String, Missing}}", "String"] =
+    @benchmarkable sumequals(nca, "D")
+
+function sumequals(A::AbstractArray, v::CategoricalValue)
+    n = 0
+    @inbounds for x in A
+        n += isequal(x, v)
+    end
+    n
 end
+
+ca = CategoricalArray(repeat(string.('A':'J'), outer=1000))
+nca = CategoricalArray(repeat([missing; string.('A':'J')], outer=1000))
+
+SUITE["isequal"]["CategoricalArray{String}", "CategoricalValue"] =
+    @benchmarkable sumequals(ca, ca[1])
+SUITE["isequal"]["CategoricalArray{Union{String, Missing}}", "CategoricalValue"] =
+    @benchmarkable sumequals(nca, nca[1])
+
+SUITE["isequal"]["CategoricalArray{String}", "CategoricalValue different pool"] =
+    @benchmarkable sumequals(ca, nca[1])
+SUITE["isequal"]["CategoricalArray{Union{String, Missing}}", "CategoricalValue  different pool"] =
+    @benchmarkable sumequals(nca, ca[1])
+
 
 # With many levels, checking whether some levels have been dropped can be very slow (#93)
-@benchgroup "CategoricalArray{String} with many levels" begin
-    a = rand([@sprintf("id%010d", k) for k in 1:1000], 10000)
-    @bench "CategoricalArray(::Vector{String})" CategoricalArray(a)
+SUITE["many levels"] = BenchmarkGroup()
 
-    a = rand([@sprintf("id%010d", k) for k in 1:1000], 10000)
-    ca = CategoricalArray(a)
-    levs = levels(ca)
-    @bench "levels! with original levels" levels!(ca, levs)
+a = rand([@sprintf("id%010d", k) for k in 1:1000], 10000)
 
-    levs = reverse(levels(ca))
-    @bench "levels! with resorted levels" levels!(ca, levs)
+SUITE["many levels"]["CategoricalArray(::Vector{String})"] =
+    @benchmarkable CategoricalArray(a)
 
-    levs = [levels(ca); [@sprintf("id2%010d", k) for k in 1:1000]]
-    @bench "levels! with many additional levels" levels!(ca, levs)
-end
+a = rand([@sprintf("id%010d", k) for k in 1:1000], 10000)
+ca = CategoricalArray(a)
 
-@benchgroup "repeated assignment" begin
-    function mycopy!(dest, src)
-        @inbounds for i in eachindex(dest, src)
-            dest[i] = src[i]
-        end
+levs = levels(ca)
+SUITE["many levels"]["levels! with original levels"] =
+    @benchmarkable levels!(ca, levs)
+
+levs = reverse(levels(ca))
+SUITE["many levels"]["levels! with resorted levels"] =
+    @benchmarkable levels!(ca, levs)
+
+levs = [levels(ca); [@sprintf("id2%010d", k) for k in 1:1000]]
+SUITE["many levels"]["levels! with many additional levels"] =
+    @benchmarkable levels!(ca, levs)
+
+
+SUITE["repeated assignment"] = BenchmarkGroup()
+
+function mycopy!(dest, src)
+    @inbounds for i in eachindex(dest, src)
+        dest[i] = src[i]
     end
-
-    a = categorical(rand([@sprintf("id%010d", k) for k in 1:1000], 10000))
-    b = CategoricalArray{String}(undef, 10000)
-    c = categorical(rand([@sprintf("id%010d", k) for k in 1:1000], 10000))
-    d = categorical(rand([@sprintf("id%010d", k) for k in 1001:2000], 10000))
-
-    @bench "empty dest" mycopy!(b2, a) setup = b2=copy(b)
-    @bench "same levels dest" mycopy!(c2, a) setup = c2=copy(c)
-    @bench "many levels dest" mycopy!(d2, a) setup = d2=copy(d)
 end
+
+a = categorical(rand([@sprintf("id%010d", k) for k in 1:1000], 10000))
+b = CategoricalArray{String}(undef, 10000)
+c = categorical(rand([@sprintf("id%010d", k) for k in 1:1000], 10000))
+d = categorical(rand([@sprintf("id%010d", k) for k in 1001:2000], 10000))
+
+SUITE["repeated assignment"]["empty dest"] =
+    @benchmarkable mycopy!(b2, a) setup = b2=copy(b)
+SUITE["repeated assignment"]["same levels dest"] =
+    @benchmarkable mycopy!(c2, a) setup = c2=copy(c)
+SUITE["repeated assignment"]["many levels dest"] =
+    @benchmarkable mycopy!(d2, a) setup = d2=copy(d)
